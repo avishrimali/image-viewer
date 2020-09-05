165f9a5b42c6e7aadef95b0baddfd86c58a2e3cd
avishrimali <avinashshrimali.85@gmail.com>
2020-09-05 15:41:52 +0530

    Adding basic folder structure and skeleton code

---
+++ src/screens/login/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./.Login.css";
+import Header from "../../common/header/Header";
+
+class Login extends Component {
+    constructor() {
+      super();
+    }
+}
+export default Login;
